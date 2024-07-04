a8978722412760e81d7c42444dbf4b0b00d3dbac
Gandalf21121 <156026172+Gandalf21121@users.noreply.github.com>
2024-07-04 19:36:20 -0400

    Delete authentication.js

--- authentication.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    var password = prompt("Ingrese la contraseña:");
-    if (password !== 'Gandalf21') {
-        alert("Contraseña incorrecta.");
-        window.location.href = "index.html";
-    }
-})();
